9aeda1d6c207b9614bbb7366b71912362ebb54b8
dufufeisdu <fufei.du@gmail.com>
2017-09-21 11:41:58 -0700

    (feature)Add public route render funcionility

--- server.py
+++ server.py
@@ -1,13 +1,13 @@
 from flask import Flask, render_template, request, redirect, jsonify, url_for, flash, g
-app = Flask(__name__)
-
 from sqlalchemy import create_engine, asc, text
 from sqlalchemy.orm import sessionmaker
 from database_setup import Base, Category, User, Item
 from flask.ext.httpauth import HTTPBasicAuth
-
-auth = HTTPBasicAuth()
 import json
+import os
+
+app = Flask(__name__)
+auth = HTTPBasicAuth()
 
 
 # Connect to Database and create database session
@@ -20,6 +20,17 @@ session = DBSession()
 
 def isLogin():
     return False
+
+
+def parseJson(arrs, cas):
+    Ites = []
+    arrsLength = len(arrs)
+    casLength = len(cas)
+    for x in xrange(arrsLength):
+        s = cas[x]
+        for y in xrange(len(arrs[x])):
+            Ites.append(arrs[x][y]["Title"] + "(" + s + ")")
+    return Ites
 
 
 @auth.verify_password
@@ -41,9 +52,11 @@ def showCategory():
   # 1th user is the public
     if isLogin() is False:
         # user_id 1 is the reserved public resource
-        with open('publicSeed.json') as data_file:
+        with open('./static/publicSeed.json') as data_file:
             data = json.load(data_file)
-            categories =
+            categories = map(lambda x: x["Name"], data["Category"])
+            items = map(lambda x: x["Item"], data["Category"])
+            items = parseJson(categories, items)
             return render_template('public.html', categories=categories, items=items)
 
     categories = session.query(Category.name, User.id).fliter(Category.name == g.user.username).
